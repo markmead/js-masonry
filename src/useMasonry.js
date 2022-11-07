2232215077528bf6476203cec64a622293e70a42
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-11-07 07:32:25 +0000

    Hotfix - Awkward cols (#2)

--- src/useMasonry.js
+++ src/useMasonry.js
@@ -1,31 +1,29 @@
-export function useMasonry(masonryGrid) {
-  const gridGap = parseFloat(getComputedStyle(masonryGrid).gap);
-  const gridCols =
-    getComputedStyle(masonryGrid).gridTemplateRows.split(" ").length;
-  const gridItems = [...masonryGrid.childNodes].filter(
+export function useMasonry(el) {
+  const gridGap = parseFloat(getComputedStyle(el).gap)
+  const gridItems = [...el.childNodes].filter(
     (gridItem) => gridItem.nodeType === 1
-  );
-  const perChunk = Math.round(gridItems.length / gridCols);
+  )
+  const perChunk = getComputedStyle(el).gridTemplateColumns.split(' ').length
 
-  gridItems.forEach((gridItem) => gridItem.style.removeProperty("margin-top"));
+  gridItems.forEach((gridItem) => gridItem.style.removeProperty('margin-top'))
 
   if (perChunk === 1) {
-    return;
+    return
   }
 
   gridItems.forEach(function (gridItem, itemIndex) {
-    const previousItem = gridItems[itemIndex - perChunk];
+    const previousItem = gridItems[itemIndex - perChunk]
 
     if (!previousItem) {
-      return;
+      return
     }
 
-    const currentItemTop = gridItem.getBoundingClientRect().top;
-    const previousItemBottom = previousItem.getBoundingClientRect().bottom;
-    const spaceBetween = currentItemTop - previousItemBottom;
+    const currentItemTop = gridItem.getBoundingClientRect().top
+    const previousItemBottom = previousItem.getBoundingClientRect().bottom
+    const spaceBetween = currentItemTop - previousItemBottom
 
     if (spaceBetween !== gridGap) {
-      gridItem.style.marginTop = `-${spaceBetween - gridGap}px`;
+      gridItem.style.marginTop = `-${spaceBetween - gridGap}px`
     }
-  });
+  })
 }
